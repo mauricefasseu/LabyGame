e9878cb3e8dfd47721d747476b397898148a4903
mauricefasseu <mauricefasseu1@web.de>
2025-04-11 23:02:32 +0200

    StartUpBean hinzugefügt, GithubAction hinzugefügt

---
+++ springweb/src/main/java/com/example/springweb/StartupBean.java
@@ -0,0 +1,24 @@
+package com.example.springweb;
+
+
+import jakarta.annotation.PostConstruct;
+import org.openapitools.client.api.DefaultApi;
+import org.openapitools.client.model.GameDto;
+import org.openapitools.client.model.GameInputDto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StartupBean {
+
+    @PostConstruct
+    public void init() {
+        DefaultApi defaultApi = new DefaultApi();
+
+        GameInputDto gameInput = new GameInputDto();
+        gameInput.setGroupName("mauricefasseu");
+
+        GameDto response = defaultApi.gamePost(gameInput);
+
+        System.out.println(response);
+    }
+}
